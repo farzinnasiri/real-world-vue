edba3a6e8cb674fd1be60d86aa9e83095cbed4b6
farzinnasiri <farzinnasiri890@gmail.com>
2020-04-15 18:40:53 +0430

    new views added

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,26 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import EventShow from "@/views/EventShow.vue";
+import EventCreate from "@/views/EventCreate.vue";
+import EventList from "@/views/EventList.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "event-list",
+    component: EventList
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/event",
+    name: "event-show",
+    component: EventShow
+  },
+  {
+    path: "/event/create",
+    name: "event-create",
+    component: EventCreate
   }
 ];
 
---
+++ src/views/EventCreate.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Creating Event</h1>
+</template>
